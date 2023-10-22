fd0b3ebc5d00f6db57eb110e256d5cc53fe512c1
matos-nelson <nmatos6@gatech.edu>
2023-10-22 16:29:53 -0500

    Implmented DocumentService.

---
+++ src/main/java/org/rent/circle/document/api/service/DocumentService.java
@@ -0,0 +1,30 @@
+package org.rent.circle.document.api.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.rent.circle.document.api.dto.FileObject;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+
+@ApplicationScoped
+public class DocumentService {
+
+    @Inject
+    S3Client s3Client;
+
+    @ConfigProperty(name = "document.bucket.name")
+    private String bucketName;
+
+    public List<FileObject> getFileListing() {
+        ListObjectsRequest listRequest = ListObjectsRequest.builder().bucket(bucketName).build();
+
+        return s3Client.listObjects(listRequest).contents().stream()
+            .map(item -> new FileObject(item.key(), item.size()))
+            .sorted(Comparator.comparing(FileObject::getKey))
+            .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/org/rent/circle/document/api/service/DocumentServiceTest.java
@@ -0,0 +1,45 @@
+package org.rent.circle.document.api.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import jakarta.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.rent.circle.document.api.dto.FileObject;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+@QuarkusTest
+public class DocumentServiceTest {
+
+    @InjectMock
+    S3Client s3Client;
+
+    @Inject
+    DocumentService documentService;
+
+    @Test
+    public void getFileListing_WhenCalled_ShouldReturnFileListing() {
+        // Arrange
+        S3Object s3Object = S3Object.builder().key("file.txt").size(100L).build();
+        ListObjectsResponse listObjectsResponse = ListObjectsResponse.builder()
+            .contents(Collections.singleton(s3Object))
+            .build();
+        when(s3Client.listObjects(Mockito.any(ListObjectsRequest.class))).thenReturn(listObjectsResponse);
+
+        // Act
+        List<FileObject> result = documentService.getFileListing();
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(1, result.size());
+    }
+}
